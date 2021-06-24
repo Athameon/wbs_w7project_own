385efd202a8c1b3913588d0e1e32f346ad21ef24
Athameon <Benjamin.Nopper@freenet.de>
2021-06-24 08:06:24 +0200

    Added a search api.

--- index.js
+++ index.js
@@ -13,6 +13,57 @@ app.use("/author", authorRouter);
 const wikiRouter = require("./routes/wikiRouter");
 app.use("/wiki", wikiRouter);
 
+app.get("/search/:key", (req, res) => {
+  const { key } = req.params;
+  console.log("key: ", key);
+  let searchResult = [];
+
+  fs.readFile("./data/authors.json", "utf-8", (error, data) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send("Failed to read the author data: " + error);
+    }
+    const authors = JSON.parse(data).items;
+    searchResult = authors.filter((author) => {
+      console.log(author.name);
+      if (author.name.includes(key)) {
+        console.log("Found author");
+      } else {
+        console.log("Author not found.");
+      }
+      return author.name.toLowerCase() === key.toLowerCase();
+    });
+
+    fs.readFile("./data/posts.json", "utf-8", (error, data) => {
+      if (error) {
+        console.log(error);
+        res.status(500).send("Failed to read the post data: " + error);
+      }
+      const posts = JSON.parse(data).items;
+      const filteredPosts = posts.filter(
+        (post) =>
+          post.title.includes(key) ||
+          (post.shortText && post.shortText.includes(key))
+      );
+      searchResult = [...searchResult, ...filteredPosts];
+
+      fs.readFile("./data/wikis.json", "utf-8", (error, data) => {
+        if (error) {
+          console.log(error);
+          res.status(500).send("Failed to read the wiki data: " + error);
+        }
+        const wikis = JSON.parse(data).items;
+        const filteredWikis = wikis.filter(
+          (wiki) => wiki.title.includes(key) || wiki.coin.includes(key)
+        );
+        searchResult = [...searchResult, ...filteredWikis];
+
+        res.send(searchResult);
+      });
+    });
+  });
+});
+
 app.all("*", (req, res) => {
   res.redirect("/");
 });
